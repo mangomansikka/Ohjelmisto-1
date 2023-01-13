c498b483f33043cdf62591cff05bcfaab5a8a8bb
Cindy Zhou <03cindy.zhou@gmail.com>
2023-01-13 11:20:28 +0200

    Task 2.6 ready.

---
+++ moduuli-02/tehtävä2.6.py
@@ -0,0 +1,8 @@
+import random
+
+koodi_1 = random.randint(0, 9), random.randint(0, 9), random.randint(0, 9)
+koodi_2 = random.randint(1, 6), random.randint(1, 6), random.randint(1, 6), random.randint(1, 6)
+
+print(koodi_1)
+
+print(koodi_2)
