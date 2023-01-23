0f58ab229d2707aa2bd0223da95b12594418234c
Cindy Zhou <03cindy.zhou@gmail.com>
2023-01-23 13:55:56 +0200

    Task 4.3 ready

--- moduuli-04/tehtävä4.3.py
+++ moduuli-04/tehtävä4.3.py
@@ -6,16 +6,14 @@ Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 """
 luvut = []
 luku = input("Anna luku: ")
-
 while luku!="":
-    luvut.append(luku)
-    print(luvut)
+    luvut.append(float(luku))
     luku = (input("Anna seuraava luku tai lopeta painamalla Enter: "))
 
 if luku=="":
     if luvut==[]:
         print("Toiminto lopetettu. ")
     else:
-        print(f"Suurin luku on " + max(luvut))
-        print(f"Pienin luku on " + min(luvut))
+        print(f"Suurin luku on " , max(luvut))
+        print(f"Pienin luku on " , min(luvut))
         print("Toiminto lopetettu. ")
