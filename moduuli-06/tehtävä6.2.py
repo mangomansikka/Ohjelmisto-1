9974089fb727a4cb1bb68853fc3820aa596175a7
Cindy Zhou <03cindy.zhou@gmail.com>
2023-01-26 15:37:17 +0200

    Task 6.2 ready

---
+++ moduuli-06/tehtävä6.2.py
@@ -0,0 +1,21 @@
+"""
+Muokkaa edellistä funktiota siten, että funktio saa parametrinaan nopan tahkojen yhteismäärän.
+Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
+Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan
+nopan maksimisilmäluku, joka kysytään käyttäjältä ohjelman suorituksen alussa.
+
+"""
+import random
+
+maksimi = int(input("Anna nopan maksimisilmäluku: "))
+def satunnainen_silmäluku():
+    return random.randint(1, maksimi)
+
+heitot = 0
+vastaus = 0
+while vastaus!=maksimi:
+    vastaus = satunnainen_silmäluku()
+    heitot = heitot + 1
+else:
+    tahkojen_summa = 21 * heitot
+    print(f"Tahkojen summa on {tahkojen_summa}. ")
