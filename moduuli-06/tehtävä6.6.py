4fe196bbcdd2d871cd26241195dc605b07258ee0
Cindy Zhou <03cindy.zhou@gmail.com>
2023-01-27 19:50:24 +0200

    Task 6.6 ready

---
+++ moduuli-06/tehtävä6.6.py
@@ -0,0 +1,28 @@
+"""
+Kirjoita funktio, joka saa parametreinaan pyöreän pizzan halkaisijan senttimetreinä sekä pizzan hinnan euroina.
+Funktio laskee ja palauttaa pizzan yksikköhinnan euroina per neliömetri.
+Pääohjelma kysyy käyttäjältä kahden pizzan halkaisijat ja hinnat sekä ilmoittaa,
+kumpi pizza antaa paremman vastineen rahalle (eli kummalla on alhaisempi yksikköhinta).
+Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+
+"""
+import math
+def pinta_ala(halkaisija, hinta):
+    halk_m = halkaisija/100
+    säde = halk_m/2
+    a = math.pi * säde**2
+    hinta_e = hinta/a
+    return a, hinta_e
+
+halk1 = int(input("Anna ensimmäisen pizzan halkaisija senttimetreinä: "))
+halk2 = int(input("Anna toisen pizzan halkaisija senttimetreinä: "))
+hinta1 = int(input("Anna ensimmäisen pizzan hinta euroina: "))
+hinta2 = int(input("Anna toisen pizzan hinta euroina: "))
+
+yksikköhinta1 = pinta_ala(halk1, hinta1)
+yksikköhinta2 = pinta_ala(halk2, hinta2)
+
+if yksikköhinta2>yksikköhinta1:
+    print("Ensimmäinen pizza antaa paremman vastineen rahalle. ")
+else:
+    print("Toinen pizza antaa paremman vastineen rahalle. ")
